dc28552d929856e24acf90f4a9d20c5e68b29d01
lyangner <lyangner--lt@yandex.ru>
2017-06-24 09:30:04 +0300

    new

--- index.php
+++ index.php
@@ -1,9 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lena
- * Date: 17.06.2017
- * Time: 10:50
- */
-$a=5;
-echo "<div style='alignment: center; '>$a </div>";
+
+
+$x=4;
+$y=1;
+$v=1;
+$k='levo';
+do {
+    $mass[$x][$y]=$v;
+    $fin=0;
+    if (!$fin AND $k=='levo') {
+        $x--;
+        if ($x<1 OR isset($mass[$x][$y])) { $x++; $k='niz'; }
+        else $fin=1;
+    }
+    if (!$fin AND $k=='niz') {
+        $y++;
+        if ($y>4 OR isset($mass[$x][$y])) { $y--; $k='pravo'; }
+        else $fin=1;
+    }
+    if (!$fin AND $k=='pravo') {
+        $x++;
+        if ($x>4 OR isset($mass[$x][$y])) { $x--; $k='verh'; }
+        else $fin=1;
+    }
+    if (!$fin AND $k=='verh') {
+        $y--;
+        if ($y<1 OR isset($mass[$x][$y])) { $y++; $k='levo'; }
+        else $fin=1;
+    }
+    if (!$fin AND $k=='levo') {
+        $x--;
+        if ($x<1 OR isset($mass[$x][$y])) { $x++; $k='niz'; }
+        else $fin=1;
+    }
+    $v++;
+} while ($v<=16);
+
+$tbl = '';
+for ($y=1;$y<=4;$y++) {
+    $tr = '';
+    for ($x=1;$x<=4;$x++) {
+        $tr .= '<td>'.$mass[$x][$y].'</td>';
+    }
+    $tbl .= '<tr>'.$tr.'</tr>';
+}
+echo '<table>'.$tbl.'</table>';
+
+?>
